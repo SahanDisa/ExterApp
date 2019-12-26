6fd93ff441dc58667fccf78b058358c0c7db638d
SahanDisa <t.sahan998@gmail.com>
2019-12-26 07:03:50 +0530

    Update main.dart to theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,26 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
+
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Welcome to Flutter',
+
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Welcome to Flutter My App'),
+          title: Text('Flix Exter'),
+          centerTitle: true,
+          backgroundColor: Colors.deepOrange[400],
         ),
         body: Center(
-          child: Text('Hello Sri Lanka Flutter\nMy Name is Sahan'),
-
-        ),
-        bottomNavigationBar: BottomAppBar(
-          shape : const CircularNotchedRectangle(),
-          child: Container(
-            height: 50.0,
-          ),
+          child: Text('New App(App to ML)'),
         ),
         floatingActionButton: FloatingActionButton(
-
-          tooltip: 'Incremnt Counter',
-          child: Icon(Icons.add),
+          child: Text('click'),
+          backgroundColor: Colors.deepOrange[200],
         ),
       ),
     );
